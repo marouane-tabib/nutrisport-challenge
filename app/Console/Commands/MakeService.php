0eb830b683a218ff14c456a5c6e8f3d45364d88c
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-11 07:42:49 +0000

    SetupProcess | Make BaseService, make:service artisan command and stub

---
+++ app/Console/Commands/MakeService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeService extends GeneratorCommand
+{
+    protected $signature = 'make:service {name}';
+
+    protected $description = 'Create a new service class';
+
+    protected $type = 'Service';
+
+    protected function getStub(): string
+    {
+        return base_path('stubs/service.stub');
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace . '\Services';
+    }
+}
