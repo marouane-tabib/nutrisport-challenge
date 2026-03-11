d75545893c0c69f30d09eababd9775c0eec23c26
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-11 07:44:44 +0000

    SetupProcess | Make CartTest feature test class

---
+++ tests/Feature/CartTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CartTest extends TestCase
+{
+    use RefreshDatabase;
+
+    // Cart tests will be added here
+}
