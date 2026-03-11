7ec2f0f3e15f3d234c5df882b8d8376d5689506d
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-11 07:39:49 +0000

    SetupProcess | Make ProductPrice model, migration, factory, seeder classes

---
+++ app/Models/ProductPrice.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductPrice extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductPriceFactory> */
+    use HasFactory;
+}
---
+++ database/factories/ProductPriceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProductPrice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ProductPrice>
+ */
+class ProductPriceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
