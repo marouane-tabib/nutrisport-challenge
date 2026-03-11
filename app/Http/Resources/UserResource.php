5d3188b779afb847e78fa0386f0fd696200896f7
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-11 23:43:28 +0000

    AddAuthProcess | Add UserResource and AgentResource API transformers

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,14 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'         => $this->id,
+            'first_name' => $this->first_name,
+            'last_name'  => $this->last_name,
+            'full_name'  => $this->full_name,
+            'email'      => $this->email,
+            'site_id'    => $this->site_id,
+            'created_at' => $this->created_at,
+        ];
     }
 }
