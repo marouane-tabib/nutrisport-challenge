0657f107384ff59d5d279e93ba7f6890ac028e4c
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-13 06:11:19 +0000

    AddCartProcess | Add AddToCartRequest with validation rules (product_id, quantity, cart_id)

---
+++ app/Http/Requests/Client/AddToCartRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Client;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class AddToCartRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'cart_id'    => 'nullable|string|uuid',
+            'product_id' => 'required|integer|exists:products,id',
+            'quantity'   => 'required|integer|min:1',
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        if (!$this->has('cart_id') || empty($this->input('cart_id'))) {
+            $this->merge(['cart_id' => (string) Str::uuid()]);
+        }
+    }
+}
