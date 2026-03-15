a71ebf1733ca75d402b33a83edf0fc13b9cd5909
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-15 01:44:52 +0000

    AddBackOfficeProcess | Add BackOffice OrderController with index, show, update status endpoints

--- app/Http/Controllers/Api/BackOffice/OrderController.php
+++ app/Http/Controllers/Api/BackOffice/OrderController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers\Api\BackOffice;
 
+use App\Exceptions\OrderException;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BackOffice\ListOrdersRequest;
+use App\Http\Resources\OrderListResource;
+use App\Services\OrderService;
 
 class OrderController extends Controller
 {
-    //
+    public function __construct(
+        protected OrderService $orderService
+    ) {}
+    
+    /**
+     * List orders from the last 5 days.
+     */
+    public function index(ListOrdersRequest $request)
+    {
+        try {
+            $orders = $this->orderService->getRecentOrders($request->validated());
+            $orders = OrderListResource::collection($orders);
+
+            return successResponse($orders);
+        } catch (OrderException $e) {
+            return errorResponse($e, 504);
+        }
+    }
 }
