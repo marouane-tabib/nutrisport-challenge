bcb6a5cec766b5fbcd0331cdbc39fcca78649368
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-15 05:40:07 +0000

    AddFeedProcess | Change ProductFeedController: add show endpoint for feed generation with format parameter

--- app/Http/Controllers/Api/Feed/ProductFeedController.php
+++ app/Http/Controllers/Api/Feed/ProductFeedController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers\Api\Feed;
 
+use App\Exceptions\ProductException;
 use App\Http\Controllers\Controller;
+use App\Services\ProductService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class ProductFeedController extends Controller
 {
-    //
+    public function __construct(
+        private ProductService $productService
+    ) {}
+
+    public function show(string $format): Response|JsonResponse
+    {
+        try {
+            $feed = $this->productService->generateFeed($format);
+
+            return feedResponse($feed['content'], $feed['content_type']);
+        } catch (ProductException $e) {
+            return errorResponse($e->getMessage(), 404);
+        }
+    }
 }
