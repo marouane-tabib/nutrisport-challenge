f8ea9cef033888fcdd0588bcb2b76c08f7e3057f
marouane-tabib <tabibmarouane6@gmail.com>
2026-03-15 01:44:09 +0000

    AddBackOfficeProcess | Add BackOffice ListOrdersRequest with validation rules for order filtering

---
+++ app/Http/Requests/BackOffice/ListOrdersRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\BackOffice;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ListOrdersRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'per_page'        => 'nullable|numeric|min:5|max:500',
+            'page'            => 'nullable|numeric',
+            'days'            => 'nullable|numeric|min:1|max:365',
+        ];
+    }
+}
